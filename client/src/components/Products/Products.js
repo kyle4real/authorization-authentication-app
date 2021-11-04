a2527fef558645c2fab823b1a4ab735a6e91c84a
Kyle Friel <businessfriel@gmail.com>
2021-11-04 10:52:17 -0400

    Input value parse and onChange parse.

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -3,9 +3,18 @@ import Form from "../UI/Form/Form";
 import Grid from "../UI/Grid/Grid";
 import List from "../UI/List/List";
 
+function numberWithCommas(x) {
+    return x?.toString()?.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const Products = () => {
+    const onSubmitHandler = (form) => {
+        form = { ...form, price: Number(form.price) };
+    };
+
     const newProductForm = (
         <Form
+            onSubmit={onSubmitHandler}
             formTitle={"Create New Product"}
             submitBtn={"Create"}
             formArr={[
@@ -19,7 +28,18 @@ const Products = () => {
                     label: "Product Price",
                     name: "price",
                     initialValue: "",
-                    type: "number",
+                    type: "text",
+                    valueParse: (value) => {
+                        return `$${numberWithCommas(value) || ""}`;
+                    },
+                    changeParse: (value) => {
+                        value = value.slice(1).split(",").join("");
+                        if (value === "") return "";
+                        if (isNaN(value)) return null;
+                        if (value.includes(".") && value.slice(value.indexOf(".") + 1).length > 2)
+                            return null;
+                        return value;
+                    },
                 },
             ]}
         />
--- client/src/components/UI/Form/Form.js
+++ client/src/components/UI/Form/Form.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import { SForm, SFormControl, SFormTitle, SInput, SLabel, SSubmitButton } from "./styles";
 
 const initialFormInput = (formArr) => {
-    return formArr.reduce((r, v) => {
-        let { name, initialValue } = v;
-        return { ...r, [name]: initialValue || "" };
-    }, {});
+    return formArr.reduce((r, v) => ({ ...r, [v.name]: v.initialValue || "" }), {});
 };
 
 const Form = ({ formArr, submitBtn, formTitle, onSubmit }) => {
     const [formInput, setFormInput] = useState(initialFormInput(formArr));
 
-    const onChangeHandler = (e) => {
-        setFormInput((p) => ({ ...p, [e.target.name]: e.target.value }));
+    const onChangeHandler = (e, changeParse) => {
+        let value = e.target.value;
+        if (changeParse) {
+            value = changeParse(value);
+            if (value === null) return;
+        }
+        setFormInput((p) => ({ ...p, [e.target.name]: value }));
     };
 
     const onSubmitHandler = (e) => {
@@ -23,8 +25,9 @@ const Form = ({ formArr, submitBtn, formTitle, onSubmit }) => {
     return (
         <SForm autoComplete="off">
             <SFormTitle>{formTitle}</SFormTitle>
-            {formArr.map(({ label, name, type }, index) => {
-                const value = formInput[name];
+            {formArr.map(({ label, name, type, valueParse, changeParse }, index) => {
+                let value = formInput[name];
+                if (valueParse) value = valueParse(value);
                 return (
                     <SFormControl key={index}>
                         <SLabel htmlFor={name}>{label}</SLabel>
@@ -33,7 +36,7 @@ const Form = ({ formArr, submitBtn, formTitle, onSubmit }) => {
                             name={name}
                             id={name}
                             value={value}
-                            onChange={onChangeHandler}
+                            onChange={(e) => onChangeHandler(e, changeParse)}
                         />
                     </SFormControl>
                 );
